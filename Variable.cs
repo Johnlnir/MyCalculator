8e788a4e9d37e8b8a56396e1e773da5f850f8a88
Johnlnir <126665836+Johnlnir@users.noreply.github.com>
2023-05-21 16:14:04 +0300

    Add files via upload

---
+++ Variable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    class Variable
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public int Tag { get; set; }
+    }
+}
